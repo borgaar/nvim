03908b0732f9b610544d2203d0b3ff4f0520034d
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2026-08-09 18:07:03 +0200

    improved install script

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 set -Eeu
 
-NEOVIM_CONFIG=$HOME/.config/nvim
-NEOVIM_BACKUP=$NEOVIM_CONFIG.backup
+NEOVIM_CONFIG="$HOME/.config/nvim"
+NEOVIM_BACKUP="$NEOVIM_CONFIG.backup"
 
 execute() {
-		echo "$1"
-		eval "$1"
+	echo "$1"
+	$1
 }
 
 log() {
-	printf -- '--> %s\n' "$1"
+	printf -- 'INFO: %s\n' "$1"
 }
 
 err() {
-	printf '!!! %s\n' "$1"
+	printf 'ERROR: %s\n' "$1"
 }
 
 restore_from_backup() {
-	err "Something went wrong. Restoring Neovim config from backup..."
+	err "Something went wrong! Restoring Neovim config from backup..."
 	execute "mv $NEOVIM_BACKUP $NEOVIM_CONFIG"
 	log "Successfully restored Neovim from backup. Exiting..."
 }
@@ -28,13 +28,13 @@ failure() {
 	err "Something went wrong! Exiting..."
 }
 
-if test -e $NEOVIM_BACKUP; then
-    echo "ERROR: Could not create backup of current Neovim config. A backup already exists at \`$NEOVIM_BACKUP\`. Either move it or delete it, then try again."
-    exit 1
+if test -e "$NEOVIM_BACKUP"; then
+	err "A backup already exists at \`$NEOVIM_BACKUP\`. Move or delete it, then try again."
+	exit 1
 fi
 
-if test -e $NEOVIM_CONFIG; then
-	mv $NEOVIM_CONFIG $NEOVIM_BACKUP
+if test -e "$NEOVIM_CONFIG"; then
+	mv "$NEOVIM_CONFIG" "$NEOVIM_BACKUP"
 	trap restore_from_backup ERR
 	log "Successfully created backup of current Neovim config at $NEOVIM_BACKUP"
 else
@@ -42,8 +42,35 @@ else
 	log "No preexisting Neovim config found. Continuing without backup..."
 fi
 
-log "Installing tree-sitter-cli"
-execute "sudo pacman -S --noconfirm tree-sitter-cli"
+arch_install() {
+	execute "sudo pacman -S --needed --noconfirm $1"
+}
+
+debian_install() {
+	execute "sudo apt-get install -y $1"
+}
+
+fedora_install() {
+	execute "sudo dnf install -y $1"
+}
+
+pkgs="git neovim tree-sitter-cli"
+
+log "Installing dependencies: $pkgs"
+if command -v pacman >/dev/null 2>&1; then
+	arch_install "$pkgs"
+
+elif command -v apt-get >/dev/null 2>&1; then
+	sudo apt-get update
+	debian_install "$pkgs"
+
+elif command -v dnf >/dev/null 2>&1; then
+	fedora_install "$pkgs"
+
+else
+	echo "Error: No supported package manager found." >&2
+	exit 1
+fi
 
 log "Installing Neovim config"
 execute "git clone https://github.com/borgaar/nvim $NEOVIM_CONFIG"
